72bf621bd7c8d38fd84695c3eaaf6ccf0f41ea9b
ManavBijlani21 <119971901+ManavBijlani@users.noreply.github.com>
2024-03-08 11:45:36 +1030

    Made new changes

---
+++ Referee.h
@@ -0,0 +1,16 @@
+#pragma once 
+using namespace std;
+#include <iostream> 
+#include "Player.h"
+#include "Human.h"
+#include "Computer.h"
+
+
+class Referee{
+
+    public:
+    Referee();
+    Player* refGame(Player* player1,Player* player2);
+
+    
+};
